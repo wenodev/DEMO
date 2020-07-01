d7d50c48571bac4bc76c8cf96dc588d4ce349671
logicstark <logicalstark1105@gmail.com>
2020-07-01 11:14:56 +0900

    fix : 구매버튼, 주문버튼 클릭 이벤트 수정

--- demo0617/src/main/java/com/demo/demo0617/controller/api/CartApiController.java
+++ demo0617/src/main/java/com/demo/demo0617/controller/api/CartApiController.java
@@ -2,22 +2,23 @@ package com.demo.demo0617.controller.api;
 
 import com.demo.demo0617.service.CartService;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
-@RestController
+@Controller
 public class CartApiController {
 
     private CartService cartService;
 
-
     @PostMapping("/cart")
     public String saveCart(Long id, String quantity){
 
+        System.out.println("id :" + id);
+        System.out.println("quantity : " + quantity);
 
 
-
+        System.out.println("called saveCart Controller");
         return "redirect:/cart";
     }
 
