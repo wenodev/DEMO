4971711aedde3c6838644272849867c0140779b1
logicstark <logicalstark1105@gmail.com>
2020-09-28 18:35:14 +0900

    fix : index페이지 404error해결

--- commerceDemo/src/main/java/com/demo/demo0617/Demo0617Application.java
+++ commerceDemo/src/main/java/com/demo/demo0617/Demo0617Application.java
@@ -1,10 +1,7 @@
 package com.demo.demo0617;
 
-import com.demo.demo0617.config.storage.StorageService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -14,14 +11,5 @@ public class Demo0617Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo0617Application.class, args);
 	}
-
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
-		};
-	}
-
 
 }
